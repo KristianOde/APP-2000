6cfbeaa4e9639f2dd29f3815455947c1d13ae2a4
Snailedlt <43886029+Snailedlt@users.noreply.github.com>
2020-04-14 18:37:53 +0200

    Basic mapp lagt til

--- client/src/GameComponents/App.js
+++ client/src/GameComponents/App.js
@@ -4,18 +4,24 @@ import BottomContainer from './Components/BottomContainer'
 import LeftContainer from './Components/LeftContainer'
 import RightContainer from './Components/RightContainer'
 import ContextContainer from './Components/ContextContainer'
+import MapContainer from './Components/MapContainer'
 import './App.css';
 
 class App extends React.Component {
   render() {
     let className = 'mainContainer';
+    let mapContainer = 'mapContainer';
     return (
-      <div className={className}>
-        <LeftContainer/>
-        <MiddleContainer/>
-        <RightContainer/>
-        <ContextContainer/>
-        <BottomContainer/>
+      <div>
+        <div className={className}>
+          <LeftContainer/>
+          <MiddleContainer/>
+          <RightContainer/>
+          <ContextContainer/>
+          <BottomContainer/>
+          <MapContainer />
+          <div className="empty"></div>
+        </div>
       </div>
     )
   }
---
+++ client/src/GameComponents/Components/MapContainer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function MapContainer(props) {
+    return (
+      <div className="MapContainer box">
+        <img src="map.png" alt="map.png" height="50%" width="100%"/> 
+        <a href="#Pleasantville">Pleasantville</a> 
+        <br />
+        <a href="#Bandit Settlement">Bandit Settlement</a> 
+        <br />
+        <a href="#The Goblin Tombs">The Goblin Tombs</a> 
+        <br />
+        <a href="#Castle of Boletaria">Castle of Boletaria</a>
+      </div>
+    )
+}
+
+export default MapContainer
